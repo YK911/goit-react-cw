1b3d71d44917c7a3fe3667ca5a1b9ed23147c56d
YK911 <jaroslavkositsa@gmail.com>
2020-02-18 10:16:44 +0200

    add lesson09

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { CSSTransition } from "react-transition-group";
+import css from "./App.module.css";
 
-function App() {
-  return <h2>DEFAULT ASSEMBLY</h2>;
+export default class App extends Component {
+  state = {
+    isOpen: false
+  };
+
+  handleOpen = () => {
+    this.setState(prev => ({
+      isOpen: !prev.isOpen
+    }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <>
+        <button onClick={this.handleOpen}>OPEN</button>
+        <CSSTransition
+          in={isOpen}
+          timeout={2000}
+          classNames={css}
+          unmountOnExit
+        >
+          <div className={css.container}>
+            <h2>show drop</h2>
+          </div>
+        </CSSTransition>
+      </>
+    );
+  }
 }
-
-export default App;
